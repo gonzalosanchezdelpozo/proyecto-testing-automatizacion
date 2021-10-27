0aad68d0959292ba7192cd7ad7cc6af882dc3a5b
Alicia Núñez <69097456+alicianunex@users.noreply.github.com>
2021-10-27 12:25:22 +0200

    Update ManufacturerServiceImpl.java

--- src/main/java/com/example/proyectotesting/service/ManufacturerServiceImpl.java
+++ src/main/java/com/example/proyectotesting/service/ManufacturerServiceImpl.java
@@ -72,5 +72,15 @@ public class ManufacturerServiceImpl implements ManufacturerService {
             return result;
         return repository.findManufacturerByDirectionCountry(country);
     }
+        @Override
+    public boolean deleteAll() {
+        try{
+            repository.deleteAll();
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    }
 
-}
