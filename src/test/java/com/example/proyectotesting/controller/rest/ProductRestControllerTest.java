c7a5bad34d8ad62889fd96520df894acd07aa833
dsansp <dsans79@yahoo.es>
2021-11-03 23:04:49 +0100

    Add DeleteTest in ProductRestControllerTest

--- src/test/java/com/example/proyectotesting/controller/rest/ProductRestControllerTest.java
+++ src/test/java/com/example/proyectotesting/controller/rest/ProductRestControllerTest.java
@@ -13,8 +13,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductRestControllerTest {
     private static final String PRODUCTSURL = "/api/products";
@@ -38,7 +38,7 @@ class ProductRestControllerTest {
  class findTests {
        @DisplayName("Comprobación buscar todos los productos")
         @Test
-       void findAll() {
+       void findAllTest() {
            createDemoProduct();
            createDemoProduct();
            createDemoProduct();
@@ -57,7 +57,7 @@ class ProductRestControllerTest {
        }
 @DisplayName("comprobamos buscar uno por ID de forma correcta")
        @Test
-       void findOneOk() {
+       void findOneOkTest() {
     Product product =createDemoProduct();
     ResponseEntity<Product> response =testRestTemplate.getForEntity(PRODUCTSURL+"/"+product.getId(), Product.class);
     assertEquals(200, response.getStatusCodeValue());
@@ -71,7 +71,7 @@ class ProductRestControllerTest {
        }
 @DisplayName("comprobamos buscar uno Not Found")
        @Test
-        void findOneNotFound(){
+        void findOneNotFoundTest(){
            ResponseEntity<Product> badDemo= testRestTemplate.getForEntity(PRODUCTSURL +"/88888", Product.class);
         assertEquals(404, badDemo.getStatusCodeValue());
         assertEquals(HttpStatus.NOT_FOUND, badDemo.getStatusCode());
@@ -84,7 +84,7 @@ class ProductRestControllerTest {
  class saveTest {
    @DisplayName("comprobamos que crea correctamente")
      @Test
-     void createOk() {
+     void createOkTest() {
        String json = """
                 {
                     "name": "Product Test",
@@ -104,7 +104,7 @@ ResponseEntity<Product> response=  testRestTemplate.postForEntity(PRODUCTSURL, c
      }
      @DisplayName("comprobamos que no crea con una badrequest")
      @Test
-     void createBadRequest() {
+     void createBadRequestTest() {
          String json = """
                 {
                     "id": 5,
@@ -121,7 +121,7 @@ ResponseEntity<Product> response=  testRestTemplate.postForEntity(PRODUCTSURL, c
      }
      @DisplayName("comprobamos que se modifica")
      @Test
-     void update() {
+     void updateOkTest() {
 
              Product product = createDemoProduct();
              String json = String.format("""
@@ -134,16 +134,127 @@ ResponseEntity<Product> response=  testRestTemplate.postForEntity(PRODUCTSURL, c
                 }
                 """, product.getId());
              System.out.println(json);
+         ResponseEntity<Product> response =
+                 testRestTemplate.exchange(PRODUCTSURL, HttpMethod.PUT, createHttpRequest(json), Product.class);
+         assertEquals(200, response.getStatusCodeValue());
+         assertEquals(HttpStatus.OK, response.getStatusCode());
+         assertTrue(response.hasBody());
+         assertNotNull(response.getBody());
+Product responseProduct = response.getBody();
+assertEquals (product.getId(),responseProduct.getId() );
+assertEquals("Product modified", responseProduct.getName());
+assertNotEquals(responseProduct.getName(), product.getName());
      }
+ @DisplayName("comprobamos update con Id null")
+     @Test
+ void updateNullTest() {
+     Product product = createDemoProduct();
+     String json = String.format("""
+                {
+                    "id": null,
+                    "name": "Product modified",
+                    "description": "description example",
+                    "quantity": 8,
+                    "price": 8.99
+                }
+                """, product.getId());
+     System.out.println(json);
+     ResponseEntity<Product> response =
+             testRestTemplate.exchange(PRODUCTSURL, HttpMethod.PUT, createHttpRequest(json), Product.class);
+     assertEquals(400, response.getStatusCodeValue());
+     assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
  }
-    @Test
-    void delete() {
-    }
+     @DisplayName("comprobamos update con Id no encontrada")
+     @Test
+     void updateIdNotFoundTest() {
+         Product product = createDemoProduct();
+         String json = String.format("""
+                {
+                    "id": 999999,
+                    "name": "Product modified",
+                    "description": "description example",
+                    "quantity": 8,
+                    "price": 8.99
+                }
+                """, product.getId());
+         System.out.println(json);
+         ResponseEntity<Product> response =
+                 testRestTemplate.exchange(PRODUCTSURL, HttpMethod.PUT, createHttpRequest(json), Product.class);
+         assertEquals(404, response.getStatusCodeValue());
+         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+     }
 
-    @Test
-    void deleteAll() {
-    }
+ }
+ @Nested
+ class deteleTests {
+    @DisplayName("comprobamos que borra correctamente con una Id")
+     @Test
+     void deleteByIdTestOk() {
+         Product product = createDemoProduct();
+         String archive = PRODUCTSURL + "/" + product.getId();
+         ResponseEntity<Product> response = testRestTemplate.getForEntity(archive, Product.class);
+         assertEquals(200, response.getStatusCodeValue());
+         assertEquals(HttpStatus.OK, response.getStatusCode());
+         assertEquals(product.getId(), response.getBody().getId());
+         testRestTemplate.delete(archive);
+         ResponseEntity<Product> response2 = testRestTemplate.getForEntity(archive, Product.class);
+         assertEquals(404, response2.getStatusCodeValue());
+         assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
+         assertFalse(response2.hasBody());
 
+     }
+@DisplayName("comprobamos que no borra con Id null")
+     @Test
+     void deleteByIdNullTest() {
+         Product product = createDemoProduct();
+               String archive = PRODUCTSURL + "/9999" + product.getId();
+         ResponseEntity<Product> response = testRestTemplate.getForEntity(archive, Product.class);
+         assertEquals(404, response.getStatusCodeValue());
+         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    ResponseEntity<Product> response2 = testRestTemplate.getForEntity(archive, Product.class);
+          testRestTemplate.delete(archive);
+          assertFalse(archive.isEmpty());
+
+
+     }
+@DisplayName("comprobamos que borramos todos")
+         @Test
+     void deleteAll() {
+             createDemoProduct();
+             createDemoProduct();
+             createDemoProduct();
+             ResponseEntity<Product[]> response = testRestTemplate.getForEntity(PRODUCTSURL, Product[].class);
+             assertNotNull(response.getBody());
+             List<Product> products = List.of(response.getBody());
+             assertTrue(products.size() >= 3);
+             testRestTemplate.delete(PRODUCTSURL);
+    response = testRestTemplate.getForEntity(PRODUCTSURL, Product[].class);
+    assertNotNull(response.getBody());
+    products = List.of(response.getBody());
+    assertEquals(0, products.size());
+     }
+
+     /*@DisplayName("comprobamos que borramos todos")
+     @Test
+     void deleteAllConflictTest() {
+         testRestTemplate.delete(PRODUCTSURL);
+         ResponseEntity<Product[]> response = testRestTemplate.exchange(PRODUCTSURL,HttpStatus.CONFLICT, createDemoProduct(), Product[].class);
+
+         List<Product> products = List.of(response.getBody());
+
+         assertTrue(products.size() == 0);
+         testRestTemplate.delete(PRODUCTSURL);
+         response = testRestTemplate.getForEntity(PRODUCTSURL, Product[].class);
+         assertTrue(products.isEmpty());
+         assertEquals(409, response.getStatusCodeValue());
+         assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+
+
+
+     }
+
+      */
+ }
 
     private Product createDemoProduct(){
         String json = """
