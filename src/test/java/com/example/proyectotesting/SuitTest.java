542ea1a62f845385b43eb5fee9075c01d8be730f
David Sans <dsans79@yahoo.es>
2021-11-03 09:20:54 +0100

    Updated fix SuitTest dependencies

--- src/test/java/com/example/proyectotesting/SuitTest.java
+++ src/test/java/com/example/proyectotesting/SuitTest.java
@@ -2,6 +2,10 @@ package com.example.proyectotesting;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 @RunWith(JUnitPlatform.class)
 
