b02d400f717a38bca1dc9f3d9ec188172db3fb8b
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-11-04 23:26:51 +0100

    CategoryRestController update delete finish but not fix

--- src/test/java/com/example/proyectotesting/controller/rest/CategoryRestControllerTest.java
+++ src/test/java/com/example/proyectotesting/controller/rest/CategoryRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.proyectotesting.controller.rest;
 
 import com.example.proyectotesting.entities.Category;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CategoryRestControllerTest {
 
-    private static final String Category_URL = "/api/categories";
+    private static final String Category_URL = "/api/category";
     private TestRestTemplate testRestTemplate;
 
     @Autowired
@@ -27,6 +28,7 @@ public class CategoryRestControllerTest {
     @LocalServerPort
     private int port;
 
+    @BeforeEach
     @Test
     void setUp() {
         restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
@@ -35,25 +37,26 @@ public class CategoryRestControllerTest {
 
 
     @Nested
-    class findTests{
+    public class findTests {
         @DisplayName("Comprobando si al buscar la categoria se encuentran todos")
         @Test
         void findAllTest() {
             //Data para Categories
             createDataCategories();
             createDataCategories();
-            createDataCategories();
+
 
             ResponseEntity<Category[]> respuesta = testRestTemplate.getForEntity(Category_URL, Category[].class);
 
-            assertEquals(200,respuesta.getStatusCodeValue());
-            assertEquals(HttpStatus.OK,respuesta.getStatusCode());
+            assertEquals(200, respuesta.getStatusCodeValue());
+            assertEquals(HttpStatus.OK, respuesta.getStatusCode());
             assertTrue(respuesta.hasBody());
             assertNotNull(respuesta.getBody());
 
             List<Category> categories = List.of(respuesta.getBody());
+
             assertNotNull(categories);
-            assertTrue(categories.size() >= 3);
+            assertTrue(categories.size() >= 2);
 
 
         }
@@ -62,6 +65,7 @@ public class CategoryRestControllerTest {
         @Test
         void findOneCategoryTest() {
             Category category = createDataCategories();
+
             ResponseEntity<Category> respuesta =
                     testRestTemplate.getForEntity(Category_URL + "/" + category.getId(), Category.class);
 
@@ -70,6 +74,7 @@ public class CategoryRestControllerTest {
             assertTrue(respuesta.hasBody());
 
             Category responseCategory = respuesta.getBody();
+
             assertNotNull(responseCategory);
             assertNotNull(responseCategory.getId());
             assertEquals(category.getId(), responseCategory.getId());
@@ -79,12 +84,12 @@ public class CategoryRestControllerTest {
         @DisplayName("Comprobamos que no encuentra el id que le pusimos")
         @Test
         void findOneDontFound() {
-            ResponseEntity<Category> response =
+            ResponseEntity<Category> responseNotFound =
                     testRestTemplate.getForEntity(Category_URL + "/777", Category.class);
 
-            assertEquals(404, response.getStatusCodeValue());
-            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-            assertFalse(response.hasBody());
+            assertEquals(404, responseNotFound.getStatusCodeValue());
+            assertEquals(HttpStatus.NOT_FOUND, responseNotFound.getStatusCode());
+            assertFalse(responseNotFound.hasBody());
         }
 
 
@@ -92,12 +97,200 @@ public class CategoryRestControllerTest {
 
 
 
-    private Category createDataCategories(){
+    @Nested
+    class createsTest {
+        @DisplayName("Comprobamos que se crea correctamente la categoria")
+        @Test
+        void createSuccesTest() {
+            String json = """
+                    {
+                        "name": " Categoria creada desde Rest Test",
+                        "color": "Color maravilloso"
+                    }
+                    """;
+            ResponseEntity<Category> respuesta = testRestTemplate.postForEntity(Category_URL, crearHttpRequest(json), Category.class);
+
+            assertEquals(201, respuesta.getStatusCodeValue());
+            assertEquals(HttpStatus.CREATED, respuesta.getStatusCode());
+            assertTrue(respuesta.hasBody());
+
+            Category category = respuesta.getBody();
+
+            assertNotNull(category);
+            assertEquals("Categoria creada desde Rest Test", category.getName());
+
+
+        }
+
+
+
+        @DisplayName("comprobamos que no crea una categoria porque hay un badrequest")
+        @Test
+        void createABadRequestTest() {
+            String json = """
+                    {
+                        "name": " Categoria BadRequest",
+                        "color": "Color maravilloso"
+                    }
+                    """;
+            ResponseEntity<Category> respuesta = testRestTemplate.postForEntity(Category_URL, crearHttpRequest(json), Category.class);
+
+            assertEquals(400, respuesta.getStatusCodeValue());
+            assertEquals(HttpStatus.BAD_REQUEST, respuesta.getStatusCode());
+            assertFalse(respuesta.hasBody());
+        }
+
+
+    }
+
+    @Nested
+    class updateTest{
+
+        @DisplayName("comprobamos  que al hacer update de esa categoria se modifica")
+        @Test
+        void updateOkTest() {
+
+            Category category = createDataCategories();
+            String json = String.format("""
+                {
+                    "id": %d,
+                    "name": "Category Modified",
+                    "color": "Green"
+                  
+                }
+                """, category.getId());
+            System.out.println(json);
+
+            ResponseEntity<Category> response =
+                    testRestTemplate.exchange(Category_URL, HttpMethod.PUT, crearHttpRequest(json), Category.class);
+
+            assertEquals(200, response.getStatusCodeValue());
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertTrue(response.hasBody());
+            assertNotNull(response.getBody());
+
+            Category responseProduct = response.getBody();
+
+            assertEquals (category.getId(),responseProduct.getId() );
+            assertEquals("Product modified", responseProduct.getName());
+            assertNotEquals(responseProduct.getName(), category.getName());
+        }
+
+        @DisplayName("comprobamos si al hacer update se mantiene la id null")
+        @Test
+        void updateNullTest() {
+            Category product = createDataCategories();
+            String json = String.format("""
+                {
+                    "id": null,
+                    "name": "Nueva Categoria Nula",
+                    "color": "un nuevo color"
+                    
+                }
+                """, product.getId());
+            System.out.println(json);
+            ResponseEntity<Category> response =
+                    testRestTemplate.exchange(Category_URL, HttpMethod.PUT, crearHttpRequest(json), Category.class);
+            assertEquals(400, response.getStatusCodeValue());
+            assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        }
+
+        @DisplayName("comprobar si al hacer update no encuentra la id que le hemos indicado")
+        @Test
+        void updateIdNotFoundTest() {
+            Category product = createDataCategories();
+            String json = String.format("""
+                {
+                    "id": 80,
+                    "name": "Category n80",
+                    "color": "color example"
+                   
+                }
+                """, product.getId());
+            System.out.println(json);
+            ResponseEntity<Category> response =
+                    testRestTemplate.exchange(Category_URL, HttpMethod.PUT, crearHttpRequest(json), Category.class);
+            assertEquals(405, response.getStatusCodeValue());
+            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        }
+
+
+
+    }
+
+    @Nested
+    class deletedTest{
+        @DisplayName("comprobamos que borra correctamente con una Id")
+        @Test
+        void deleteByIdSuccesTest() {
+            Category category = createDataCategories();
+            String archive = Category_URL + "/" + category.getId();
+            ResponseEntity<Category> response = testRestTemplate.getForEntity(archive, Category.class);
+
+            assertEquals(200, response.getStatusCodeValue());
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals(category.getId(), response.getBody().getId());
+
+            testRestTemplate.delete(archive);
+
+            ResponseEntity<Category> response2 = testRestTemplate.getForEntity(archive, Category.class);
+
+            assertEquals(404, response2.getStatusCodeValue());
+            assertEquals(HttpStatus.NOT_FOUND, response2.getStatusCode());
+            assertFalse(response2.hasBody());
+
+        }
+        @DisplayName("comprobamos que no se borra una categoria con una  Id null")
+        @Test
+        void deleteNullIdTest() {
+            Category category = createDataCategories();
+
+            String archive = Category_URL + "/9999" + category.getId();
+
+            ResponseEntity<Category> response = testRestTemplate.getForEntity(archive, Category.class);
+
+            assertEquals(404, response.getStatusCodeValue());
+            assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+            ResponseEntity<Category> response2 = testRestTemplate.getForEntity(archive, Category.class);
+            testRestTemplate.delete(archive);
+
+            assertFalse(archive.isEmpty());
+
+
+        }
+        @DisplayName("comprobamos que borramos todos")
+        @Test
+        void deleteAllSuccess() {
+            createDataCategories();
+            createDataCategories();
+
+            ResponseEntity<Category[]> response = testRestTemplate.getForEntity(Category_URL, Category[].class);
+
+            assertNotNull(response.getBody());
+
+            List<Category> categories = List.of(response.getBody());
+
+            assertTrue(categories.size() >= 2);
+
+            testRestTemplate.delete(Category_URL);
+            response = testRestTemplate.getForEntity(Category_URL, Category[].class);
+
+            assertNotNull(response.getBody());
+
+            categories = List.of(response.getBody());
+            assertEquals(0, categories.size());
+        }
+    }
+
+
+
+    private Category createDataCategories() {
         String json = """
                 {
                     "name": " Categoria creada desde Rest",
                     "color": "Color maravilloso"
-              
+                              
                 }
                 """;
         HttpHeaders headers = new HttpHeaders();
@@ -109,5 +302,11 @@ public class CategoryRestControllerTest {
         return response.getBody();
     }
 
-
+    private HttpEntity<String> crearHttpRequest(String json) {
+        HttpHeaders cabeceras = new HttpHeaders();
+        cabeceras.setContentType(MediaType.APPLICATION_JSON);
+        cabeceras.setAccept(List.of(MediaType.APPLICATION_JSON));
+        HttpEntity<String> request = new HttpEntity<>(json, cabeceras);
+        return request;
+    }
 }
