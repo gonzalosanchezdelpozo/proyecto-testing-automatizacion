1f2bd9fdedeb26984c907f99642e785be9228936
Ismael Gonzalez <ismamontiel@hotmail.com>
2021-11-04 20:19:14 +0100

    manufacturer test

--- src/test/java/com/example/proyectotesting/service/ManufacturerServiceImplTest.java
+++ src/test/java/com/example/proyectotesting/service/ManufacturerServiceImplTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -22,38 +24,60 @@ class ManufacturerServiceImplTest {
        manufacturerRepository = mock(ManufacturerRepository.class);
        this.manufacturerService = new ManufacturerServiceImpl(manufacturerRepository);
 
-       List<Manufacturer> arrayList = new ArrayList<>();
-       arrayList.add(new Manufacturer());
-       arrayList.add(new Manufacturer());
+        List<Manufacturer> manufacturers = Arrays.asList(
+                new Manufacturer("name1","cif1", 5,2021),
+                new Manufacturer("name2", "cif2", 3, 2019));
 
-       when(manufacturerRepository.findAll()).thenReturn(arrayList);
-
-    }
-
-
-
-    @Test
-    void findAll() {
-
-        List<Manufacturer> manufacturers = manufacturerService.findAll();
-
-    }
-
-    @Test
-    void findOne() {
-    }
-
-    @Test
-    void findByYear() {
-    }
-
-    @Test
-    void save() {
     }
 
     @Test
     void count() {
+        //configuracion escenario
+        when(manufacturerRepository.count()).thenReturn(2L);
+        //ejecutar el comportamiento a testear
+        Long result= manufacturerService.count();
+        //Aserciones y verificaciones
+        assertNotNull(result);
+        assertEquals(2,result);
     }
+
+    @Test
+    void findAll() {
+        List<Manufacturer> manufacturers = Arrays.asList(
+                new Manufacturer("name1","cif1", 5,2021),
+                new Manufacturer("name2", "cif2", 3, 2019));
+
+        when(manufacturerRepository.findAll()).thenReturn(manufacturers);
+
+        List<Manufacturer> result = manufacturerService.findAll();
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        verify(manufacturerRepository).findAll();
+    }
+
+    @Test
+    void findOne() {
+
+        Optional<Manufacturer> manufacturer = manufacturerService.findOne(1L);
+        assertNotNull(manufacturer);
+        assertEquals(1L, manufacturer.get().getId());
+
+    }
+
+    @Test
+    void findByYear() {
+      //  List<Manufacturer> manufacturers = );
+        //assertNotNull(manufacturers);
+        //ssertTrue(manufacturers > );
+    }
+
+    @Test
+    void save() {
+        //Manufacturer manufacturer = manufacturerService.save();
+
+    }
+
+
 
     @Test
     void deleteById() {
