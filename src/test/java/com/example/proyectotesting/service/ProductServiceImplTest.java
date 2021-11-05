c5652deb7357a879855d93c6771559519c23a0c2
dsansp <dsans79@yahoo.es>
2021-11-05 23:30:57 +0100

    Add FindTest for ProductServiceImplTest

--- src/test/java/com/example/proyectotesting/service/ProductServiceImplTest.java
+++ src/test/java/com/example/proyectotesting/service/ProductServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.proyectotesting.service;
 
+import com.example.proyectotesting.entities.Direction;
 import com.example.proyectotesting.entities.Manufacturer;
 import com.example.proyectotesting.entities.Product;
 import com.example.proyectotesting.repository.ManufacturerRepository;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -72,15 +74,20 @@ class ProductServiceImplTest {
            assertEquals(Optional.empty(), product);
        }
        @Test
-       void findByIdTest() {
+       void OptionalfindByIdTest() {
+          List<Product> products =new ArrayList<>();
            Manufacturer made = new Manufacturer();
-           List<Product> products = Arrays.asList(
-                   new Product("Balón", "futbol", 2, 10.99, made),
-                   new Product("Balón2", "basket", 3, 16.99, made));
-           when( productRepository.findAll()).thenReturn(products);
+
+           Product product1= new Product("Balón", "futbol", 2, 10.99, made);
+           System.out.println(products);
+           product1.setId(1L);
+           products.add(product1);
+           assertEquals(1,products.size());
+           System.out.println(product1);
            Optional<Product> product = productService.findOne(1L);
            assertNotNull(product);
-           assertEquals(1L,product.get().getId());
+           assertTrue(product.isEmpty());
+
        }
 
        @Test
@@ -93,7 +100,7 @@ class ProductServiceImplTest {
         Boolean product=productService.existsById(1L);
         assertNotNull(product);
         assertTrue(product);
-
+           System.out.println();
        }
        @Test
        void NotexistsByIdTest() {
@@ -107,19 +114,65 @@ class ProductServiceImplTest {
 
        }
        @Test
-       void findByPriceBetween() {
+       void findByPriceBetweenNullTest() {
+           Double pricemin = 9.99;
+           Double pricemax = 14.99;
+           Manufacturer made = new Manufacturer();
+           List<Product> products = new ArrayList<>();
+           Product product1 = new Product("Balón", "futbol", 2, 10.99, made);
+           product1.setId(1L);
+
+           products.add(product1);
+           Product product2 = (new Product("Balón2", "basket", 3, 16.99, made));
+           product1.setId(1L);
+
+           products.add(product2);
+           System.out.println(products);
+           when(productService.findByPriceBetween(pricemin, pricemax)).thenReturn(null);
+           List<Product> prices = productService.findByPriceBetween(pricemin, pricemax);
+           System.out.println(prices);
+           assertNull(prices);
        }
 
-       @Test
-       void findByManufacturer() {
-       }
 
        @Test
-       void calculateShippingCost() {
+       void findByManufacturerEmptyTest() {
+
+           List<Product> manufacturer= productService.findByManufacturer(null);
+           assertTrue(manufacturer.isEmpty());
+       }
+
+
+       @Test
+       void calculateShippingCostNullTest() {
+           Manufacturer made = new Manufacturer();
+           Product product1= new Product("Balón", "futbol", 2, 10.99, made);
+           Direction direction1 = new Direction(null, "33010", "León", "Spain");
+            Double shipping= productService.calculateShippingCost(null,direction1);
+            assertEquals(0d, shipping);
+       }
+       @Test
+       void calculateShippingCostSpainTest() {
+           Manufacturer made = new Manufacturer();
+           Product product1= new Product("Balón", "futbol", 2, 10.99, made);
+           Direction direction1 = new Direction("Calle melancolia", "33010", "León", "Spain");
+           Double shipping= productService.calculateShippingCost(product1,direction1);
+           assertEquals(2.99d, shipping);
+
+       }
+       @Test
+       void calculateShippingCostPOutSpainTest() {
+           Manufacturer made = new Manufacturer();
+           Product product1= new Product("Balón", "futbol", 2, 10.99, made);
+           Direction direction1 = new Direction("liberty strees", "33010", "utah", "USA");
+           Double shipping= productService.calculateShippingCost(product1,direction1);
+           assertEquals(22.990000000000002, shipping);// bug decimales
+
        }
    }
+   /*
     @Test
-    void save() {
+    void save(Manufacturer artengo) {
     }
 
 
@@ -131,6 +184,6 @@ class ProductServiceImplTest {
     @Test
     void deleteAll() {
     }
-
+*/
 
     }
