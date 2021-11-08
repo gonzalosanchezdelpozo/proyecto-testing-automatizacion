8061011964866e90029f47eac28c00c813d87ccb
Ismael Gonzalez <ismamontiel@hotmail.com>
2021-11-08 21:29:43 +0100

    manufacturer controller rest test

--- src/test/java/com/example/proyectotesting/service/ManufacturerServiceImplTest.java
+++ src/test/java/com/example/proyectotesting/service/ManufacturerServiceImplTest.java
@@ -2,7 +2,6 @@ package com.example.proyectotesting.service;
 
 import com.example.proyectotesting.entities.Manufacturer;
 import com.example.proyectotesting.repository.ManufacturerRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -32,17 +31,15 @@ class ManufacturerServiceImplTest {
 
     @Test
     void count() {
-        //configuracion escenario
         when(manufacturerRepository.count()).thenReturn(2L);
-        //ejecutar el comportamiento a testear
         Long result= manufacturerService.count();
-        //Aserciones y verificaciones
+
         assertNotNull(result);
         assertEquals(2,result);
     }
 
     @Test
-    void findAll() {
+    void findAllTest() {
         List<Manufacturer> manufacturers = Arrays.asList(
                 new Manufacturer("name1","cif1", 5,2021),
                 new Manufacturer("name2", "cif2", 3, 2019));
@@ -56,38 +53,83 @@ class ManufacturerServiceImplTest {
     }
 
     @Test
-    void findOne() {
+    void findOneIdTest() {
+        List<Manufacturer> manufacturers = new ArrayList<>();
+
+        Manufacturer manufacturer1 = new Manufacturer("name1", "12345", 4, 2007);
+        manufacturer1.setId(1L);
+        manufacturers.add(manufacturer1);
+
+        assertEquals(1, manufacturers.size());
 
         Optional<Manufacturer> manufacturer = manufacturerService.findOne(1L);
         assertNotNull(manufacturer);
-        assertEquals(1L, manufacturer.get().getId());
-
+        assertTrue(manufacturer.isEmpty());
     }
 
     @Test
-    void findByYear() {
-      //  List<Manufacturer> manufacturers = );
-        //assertNotNull(manufacturers);
-        //ssertTrue(manufacturers > );
+    void findOneNullTest() {
+        Optional<Manufacturer> manufacturer = manufacturerService.findOne((Long) null);
+        assertTrue(manufacturer.isEmpty());
     }
 
     @Test
-    void save() {
-        //Manufacturer manufacturer = manufacturerService.save();
+    void findByYearTest() {
+        List<Manufacturer> manufacturer = manufacturerService.findByYear(2021);
+        assertNotNull(manufacturer);;
+    }
 
+    @Test
+    void findYearNullTest() {
+
+        List<Manufacturer> manufacturer =
+                manufacturerService.findByYear(null);
+        assertTrue(manufacturer.isEmpty());
+    }
+
+    @Test
+    void findYearEmptyTest() {
+
+        List<Manufacturer> manufacturer =
+                manufacturerService.findByYear(0);
+        assertTrue(manufacturer.isEmpty());
+    }
+
+    @Test
+    void saveOKTest() {
+        when(manufacturerRepository.save(any(Manufacturer.class)))
+                .thenReturn(new Manufacturer());
+        Manufacturer manufacturer = manufacturerService.save(new Manufacturer());
+        assertNotNull(manufacturer);
+    }
+
+    @Test
+    void saveNullTest() {
+        assertNull(manufacturerService.save(null));
     }
 
 
 
     @Test
     void deleteById() {
+    //when(manufacturerService.deleteById(1L)
+      //      .thenReturn(manufacturerService.deleteById(1L);
+    //verify(manufacturerRepository).deleteById(1L);
     }
 
     @Test
-    void findManufacturerByCountry() {
+    void findManufacturerByCountryTest() {
+
+    List<Manufacturer> manufacturers = manufacturerRepository.findManufacturerByDirectionCountry("España");
+        assertNotNull(manufacturers);
+        verify(manufacturerRepository).findManufacturerByDirectionCountry("España");
+
     }
 
     @Test
     void deleteAll() {
+        manufacturerService.deleteAll();
+        verify(manufacturerRepository).deleteAll();
+
     }
 }
