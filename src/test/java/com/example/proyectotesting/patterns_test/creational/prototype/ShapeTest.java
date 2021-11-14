5a0ca05c8b18bfdcab379c0be9918035258feabc
María de los Ángeles Barba <mangelesbarma@gmail.com>
2021-11-14 20:28:32 +0000

    ShapeTest y DirectionRestControllerTest

--- src/test/java/com/example/proyectotesting/patterns_test/creational/prototype/ShapeTest.java
+++ src/test/java/com/example/proyectotesting/patterns_test/creational/prototype/ShapeTest.java
@@ -1,25 +1,41 @@
 package com.example.proyectotesting.patterns_test.creational.prototype;
 
+import com.example.proyectotesting.patterns.creational.prototype.Shape;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+@Disabled
+ class ShapeTest extends Shape {
+    String color;
 
-class ShapeTest {
+    protected ShapeTest(String color) {
+        super(color);
+    }
+
+     public Shape copy(){
+        Shape shape = null;
+        return (shape);
+     }
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void getColor() {
+    void getColorTest() {
+        String colorReturn;
+        ShapeTest shapeTest = new ShapeTest("rojo");
+        colorReturn = shapeTest.getColor();
+        assertEquals("rojo", colorReturn);
     }
 
     @Test
-    void setColor() {
+    void setColorTest() {
     }
 
     @Test
-    void copy() {
+    void copyTest() {
     }
 }
