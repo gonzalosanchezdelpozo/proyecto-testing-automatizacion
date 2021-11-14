2bd5fa68d34091aefbe544c86ddbf1addc4bfbcd
María de los Ángeles Barba <mangelesbarma@gmail.com>
2021-11-14 20:32:00 +0000

    Main de prototype\shopcart

--- src/test/java/com/example/proyectotesting/patterns_test/creational/prototype/shopcart/MainTest.java
+++ src/test/java/com/example/proyectotesting/patterns_test/creational/prototype/shopcart/MainTest.java
@@ -1,17 +1,33 @@
 package com.example.proyectotesting.patterns_test.creational.prototype.shopcart;
 
+import com.example.proyectotesting.entities.Product;
+import com.example.proyectotesting.patterns.behavioral.strategy.ShopCart;
+import com.example.proyectotesting.patterns.structural.adapter.Main;
+import com.example.proyectotesting.repository.DirectionRepository;
+import com.example.proyectotesting.service.DirectionServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class MainTest {
-
+    Product juego;
+    ShopCart cart;
     @BeforeEach
     void setUp() {
+        juego = mock(Product.class);
+        this.cart = new ShopCart();
     }
 
     @Test
     void main() {
+        //when((cart.addProduct(juego))).thenReturn(true);
+
+        Main.main(new String[] {});
+
+        //assertTrue(cart.addProduct(juego));
     }
 }
